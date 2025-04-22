0254d7ae65847b61c075e5aecf66aabef87262a3
Math-Baba <mathieu.rl.baba@gmail.com>
2025-04-22 15:16:42 +0400

    Test

--- turtle_regulation/turtle_regulation/set_way_point.py
+++ turtle_regulation/turtle_regulation/set_way_point.py
@@ -1,33 +1,59 @@
 import rclpy
 from rclpy.node import Node
 from turtlesim.msg import Pose
-from math import sqrt, pi, atan2, tan
-
-class BasicSubscriber(Node):
+from geometry_msgs.msg import Twist
+from math import atan2, tan, pi, atan
+ 
+class Publisher(Node):
     def __init__(self):
-        super().__init__('set_way_point')
+        super().__init__('move_towards_waypoint')
+ 
         self.current_pose = None
-        self.waypoint = [7.0, 7.0] 
+        self.waypoint = [7.0, 7.0]
+ 
         self.subscription = self.create_subscription(
             Pose,
-            '/turtle1/pose',
+            '/turtle1/pose',  
             self.update_pose,
             10)
-       
-        
-
+ 
+        self.publisher = self.create_publisher(
+            Twist,
+            '/turtle1/cmd_vel',
+            10)
+ 
+        self.timer = self.create_timer(0.3, self.publisher_callback)
+ 
     def update_pose(self, msg):
         self.current_pose = msg
         x = msg.x
         y = msg.y   
+        
+        
+ 
+    def publisher_callback(self):
+        if self.current_pose is None:
+            return
+ 
+        x = self.current_pose.x
+        y = self.current_pose.y
+        theta = self.current_pose.theta
+ 
         angle = atan2(self.waypoint[1] - self.current_pose.y, self.waypoint[0] - self.current_pose.x)
-        self.get_logger().info(
-            f'Position actuelle : x={x:.2f}, y={y:.2f}, theta={msg.theta:.2f} | angle vers waypoint = {angle:.2f} rad'
-        )
+        self.get_logger().info(f"Angle : {angle}")
 
-def main():
-    rclpy.init()
-    node = BasicSubscriber()
+        e = atan(tan(angle - self.current_pose.theta)/2)
+        self.get_logger().info(f"Erreur angulaire :{e}")
+ 
+        msg = Twist()
+        Kp = 0.05
+        msg.angular.z = Kp * e
+        self.get_logger().info(f"Vitesse : {msg.angular.z}")
+        self.publisher.publish(msg)
+ 
+def main(args=None):
+    rclpy.init(args=args)
+    node = Publisher()
     rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
