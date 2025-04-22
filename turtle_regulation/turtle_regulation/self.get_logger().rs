aeb085058dcafe8e6029ffd548955bddefa4089c
Math-Baba <mathieu.rl.baba@gmail.com>
2025-04-22 16:29:52 +0400

    Arrêt de la tortue lorsqu'elle atteint le waypoint (distance tolérance)

---
+++ turtle_regulation/turtle_regulation/self.get_logger().rs
@@ -0,0 +1,2 @@
+self.get_logger().info(f"Erreur angulaire :{e}")
+self.get_logger().info(f"Angle : {angle}")
--- turtle_regulation/turtle_regulation/set_way_point.py
+++ turtle_regulation/turtle_regulation/set_way_point.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node
 from turtlesim.msg import Pose
 from geometry_msgs.msg import Twist
-from math import atan2, tan, pi, atan
+from math import atan2, atan, tan, pi, sqrt
  
 class Publisher(Node):
     def __init__(self):
@@ -22,13 +22,13 @@ class Publisher(Node):
             '/turtle1/cmd_vel',
             10)
  
-        self.timer = self.create_timer(0.3, self.publisher_callback)
+        self.timer = self.create_timer(0.1, self.publisher_callback)
  
     def update_pose(self, msg):
         self.current_pose = msg
         x = msg.x
-        y = msg.y   
-        
+        y = msg.y  
+        z = msg.theta
         
  
     def publisher_callback(self):
@@ -36,21 +36,33 @@ class Publisher(Node):
             return
  
         x = self.current_pose.x
+        #self.get_logger().info(f"Pose x : {x}")
         y = self.current_pose.y
+        #self.get_logger().info(f"Pose y : {y}")
         theta = self.current_pose.theta
  
         angle = atan2(self.waypoint[1] - self.current_pose.y, self.waypoint[0] - self.current_pose.x)
-        self.get_logger().info(f"Angle : {angle}")
-
+        #self.get_logger().info(f"Angle : {angle}")
+ 
         e = atan(tan(angle - self.current_pose.theta)/2)
-        self.get_logger().info(f"Erreur angulaire :{e}")
+        #self.get_logger().info(f"Erreur angulaire :{e}")
+ 
+        e_l = sqrt((self.waypoint[1] - y) ** 2 + (self.waypoint[0] - x) ** 2)
+        self.get_logger().info(f"Erreur linéaire : {e_l}")
+        
+        distance_tolerance =0.3
+        
+        if e_l < distance_tolerance:
+            return
  
         msg = Twist()
-        Kp = 0.05
+        Kpl = 0.7
+        Kp = 1.0
         msg.angular.z = Kp * e
-        self.get_logger().info(f"Vitesse : {msg.angular.z}")
+        msg.linear.x = Kpl * e_l
         self.publisher.publish(msg)
  
+        
 def main(args=None):
     rclpy.init(args=args)
     node = Publisher()
